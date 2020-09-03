05bc8c8743bed9f8a151e72dab8b37013cbdb288
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2020-09-03 21:33:45 +0800

    added CFG.scala

---
+++ src/main/scala/com/github/luzhuomi/obsidian/CFG.scala
@@ -0,0 +1,13 @@
+package com.github.luzhuomi.obsidian
+
+
+import com.github.luzhuomi.scalangj.Syntax._
+/*
+ Control Flow Graph construction
+ */
+
+ object CFG {
+
+    case class Node( stmt:List[BlockStmt] ) 
+
+ }
