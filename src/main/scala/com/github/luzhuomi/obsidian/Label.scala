fc0c6a688da4a5335f931462e3682cc0b294ce55
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2021-01-24 21:04:51 +0800

    updated

--- src/main/scala/com/github/luzhuomi/obsidian/Label.scala
+++ src/main/scala/com/github/luzhuomi/obsidian/Label.scala
@@ -39,60 +39,8 @@ package com.github.luzhuomi.obsidian
   * 
   * A simple state monad should serve the purpose
   * 
-  * We use the judgetment l |- stmt => stmt, 
-  * _ denotes a none label
   * 
-  * 
-  * -----------------------(BreakNone)
-  * l |- break => break l
-  * 
-  *
-  * -------------------------(BreakSome)
-  * l |- break l' => break l' 
-  * 
-  * -----------------------(ContinueNone)
-  * l |- continue => continue l
-  * 
-  * 
-  * -----------------------(ContinueSome)
-  * l |- continue l' => continue l'
-  * 
-  * 
-  * l' is fresh
-  * l' |- stmt1 => stmt1' ... l' |- stmtn => stmtn' 
-  * ----------------------------------------------------------------------------------------------------------------------(Switch)
-  * l |- switch e ( case c1: stmt1; ; case c2: stmt2; ... )  => l' : switch e ( case c1: stmt1'; ; case c2: stmt2'; ... ) 
-  * 
-  * 
-  * 
-  * l' is fresh
-  * l' |- stmt => stmt'
-  * ------------------------------------------------(While)
-  * l |- while (e) { stmt } => while (e) { stmt' }
-  * 
-  * 
-  * 
-  * --------------------(Assignment)
-  * l |- x = e => x = e
-  * 
-  * 
-  * l |- stmt1 => stmt1'  
-  * l |- stmt2 => stmt2'
-  * ------------------------------------------(Seq)
-  * l |- stmt1; stmt2 => stmt1'; stmt2';
-  * 
-  *  l' |- stmt => stmt' 
-  * ----------------------------------------(Label)
-  * l |- l' : stmt => l' : stmt'
-  * 
-  * l |- stmt1 => stmt1'
-  * l |- stmt2 => stmt2'
-  * l |- stmt3 => stmt3'
-  * --------------------------------------------------------------------------------------------------------------------(Try)
-  * l |- try { stmt1 } catch (T e) { stmt2 } finally {stmt3} => try { stmt1' } catch (T e) { stmt2' } finally {stmt3'}
-  * 
-  * 
-  * hm... we need to keep track of two labels? because we might have a case
+  *  Wait! we need to keep track of two labels? because we might have a case
   * 
   * while (e) {
   *   switch (e2) {
@@ -109,6 +57,68 @@ package com.github.luzhuomi.obsidian
   *         case c1:
   *           continue l1
   * }
+
+  * 
+  * 
+  * We use the judgetment l_c, l_b |- stmt => stmt, 
+  * l_c is a continuable label, l_b is a breakable label,   when we don't care a label in the context, we use _
+  * 
+  * 
+  * -----------------------(BreakNone)
+  * l_c, l_b |- break => break l_b
+  * 
+  *
+  * -------------------------(BreakLabel)
+  * l_c, l_b |- break l' => break l' 
+  * 
+  * -----------------------(ContinueNone)
+  * l_c, l_b |- continue => continue l_c
+  * 
+  * 
+  * -----------------------(ContinueLabel)
+  * l_c, l_b |- continue l' => continue l'
+  * 
+  * 
+  * l_b' is fresh
+  * l_c, l_b' |- stmt1 => stmt1' ... l' |- stmtn => stmtn' 
+  * ----------------------------------------------------------------------------------------------------------------------(SwitchNone)
+  * l_c, _ |- switch e ( case c1: stmt1; ; case c2: stmt2; ... )  => l_b' : switch e ( case c1: stmt1'; ; case c2: stmt2'; ... ) 
+  * 
+  * l_c, l_b' |- stmt1 => stmt1' ... l' |- stmtn => stmtn' 
+  * ----------------------------------------------------------------------------------------------------------------------(SwitchLabel)
+  * l_c, _ |- l_b' : switch e ( case c1: stmt1; ; case c2: stmt2; ... )  => l_b' : switch e ( case c1: stmt1'; ; case c2: stmt2'; ... ) 
+  * 
+  * 
+  * l' is fresh
+  * l', l' |- stmt => stmt'
+  * ----------------------------------------------------------------------------------(WhileNone)
+  * _, _ |- while (e) { stmt } => while (e) { stmt' } => l' : while (e) { stmt' } 
+  * 
+  * 
+  * l', l' |- stmt => stmt'
+  * ----------------------------------------------------------------------------------(WhileLabel)
+  * _, _ |- l' : while (e) { stmt } => while (e) { stmt' } => l' : while (e) { stmt' } 
+  * 
+  * --------------------(Assignment)
+  * l |- x = e => x = e
+  * 
+  * 
+  * l_c, l_b |- stmt1 => stmt1'  
+  * l_c, l_b |- stmt2 => stmt2'
+  * ------------------------------------------(Seq)
+  * l_c, l_b |- stmt1; stmt2 => stmt1'; stmt2';
+  * 
+  * l_c, l_b |- stmt => stmt' 
+  * ----------------------------------------(Label)  do we need this case?
+  * l_c, l_b |- l' : stmt => l' : stmt'
+  * 
+  * l_c, l_b |- stmt1 => stmt1'
+  * l_c, l_b |- stmt2 => stmt2'
+  * l_c, l_b |- stmt3 => stmt3'
+  * --------------------------------------------------------------------------------------------------------------------(Try)
+  * l_c, l_b |- try { stmt1 } catch (T e) { stmt2 } finally {stmt3} => try { stmt1' } catch (T e) { stmt2' } finally {stmt3'}
+  * 
+  * 
   * 
   * */
 
