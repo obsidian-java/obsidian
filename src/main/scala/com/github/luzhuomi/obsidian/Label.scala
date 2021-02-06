0405e8c79094939a72057ecd6d7099fea9b0a757
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2021-02-06 23:07:48 +0800

    testing Label.scala

--- src/main/scala/com/github/luzhuomi/obsidian/Label.scala
+++ src/main/scala/com/github/luzhuomi/obsidian/Label.scala
@@ -132,12 +132,19 @@ import com.github.luzhuomi.scalangj.Syntax
 import com.github.luzhuomi.obsidian.ASTUtils._
 object Label {
 
+  /**
+    * State object of the label monad
+    *
+    * @param currNum - current unused number
+    * @param labelPrefix - label prefix to be used in generating label
+    */
   case class StateInfo(
-    currNum:Int
+    currNum:Int,
+    labelPrefix:String 
   )
 
 
-  val initStateInfo = StateInfo(0)
+  val initStateInfo = StateInfo(0, "obsLbl")
 
   sealed trait LabelResult[+A] 
   case class LabelError(msg:String) extends LabelResult[Nothing]
@@ -184,6 +191,163 @@ object Label {
   def get: StateT[LabelResult, StateInfo, StateInfo] = StateT{ st => LabelOk(st, st) }
   def put(st:StateInfo): StateT[LabelResult, StateInfo, Unit] = StateT{ _ => LabelOk(st, ())}
 
-	type LabelState[A] = StateT[LabelResult, StateInfo,A]
 
+  type LabelState[A] = StateT[LabelResult, StateInfo,A]
+
+
+  def newLabel:LabelState[Ident] = for {
+    st <- get
+    _  <- put(st.copy(currNum= st.currNum+1))
+  } yield Ident(s"${st.labelPrefix}_${st.currNum}")
+
+  
+  trait Label[A] {
+    /**
+      * 
+      *
+      * @param a
+      * @param labelC - continable label
+      * @param labelB - breakable label
+      * @param m
+      * @return
+      */
+    def label(a:A, labelB:Option[Ident], labelC:Option[Ident])(implicit m:MonadError[LabelState, String]):LabelState[A]
+  }
+
+  object labelOps {
+    def label[A](a:A, labelB:Option[Ident], labelC:Option[Ident])(implicit d:Label[A], m:MonadError[LabelState, String]):LabelState[A] = {
+      d.label(a,labelB, labelC)(m)
+    }
+  }
+
+  implicit def methodDeclLabelInstance:Label[MethodDecl] = new Label[MethodDecl] {
+    override def label(a: Syntax.MethodDecl, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.MethodDecl] = a match {
+      case MethodDecl(modifier, type_params, ty, id, formal_params, ex_types, exp, body) => for {
+        l_body <- labelOps.label(body,labelB, labelC) 
+      } yield MethodDecl(modifier, type_params, ty, id, formal_params, ex_types, exp, l_body)
+    }
+  }
+
+  implicit def methodBodyLabelInstance:Label[MethodBody] = new Label[MethodBody] {
+    override def label(a: Syntax.MethodBody, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.MethodBody] = a match {
+      case MethodBody(None) => m.pure(a)
+      case MethodBody(Some(blk)) => for {
+        l_blk      <- labelOps.label(blk, labelB, labelC)
+      } yield MethodBody(Some(l_blk))
+    }
+  }
+
+  implicit def blockLabelInstance:Label[Block] = new Label[Block] {
+    override def label(a: Syntax.Block, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.Block] = a match {
+      case Block(blk_stmts) => for {
+        l_blk_stmts <- blk_stmts.traverse(labelOps.label(_,labelB, labelC))
+      } yield Block(l_blk_stmts)
+    }
+  }
+
+  implicit def blockStmtLabelInstance:Label[BlockStmt] = new Label[BlockStmt] {
+    override def label(a: Syntax.BlockStmt, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.BlockStmt] = a match {
+      case BlockStmt_(stmt) => for {
+        l_stmt <- labelOps.label(stmt, labelB, labelC)
+      } yield BlockStmt_(l_stmt)
+      case LocalClass(class_decl) => m.pure(a) // we don't label nested class for now
+      case LocalVars(modifiers, ty, var_decls) => m.pure(a)
+    }
+  }
+
+  implicit def stmtLabelInstance:Label[Stmt] = new Label[Stmt] {
+    override def label(a: Syntax.Stmt, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.Stmt] = a match {
+      case Assert(exp, msg) => m.pure(a)
+      case Labeled(id, stmt) => stmt match {
+        case BasicFor(init, loop_cond, post_update, stmt) => for {
+          l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+        } yield Labeled(id, BasicFor(init, loop_cond, post_update, l_stmt))
+        case EnhancedFor(modifiers, ty, id, exp, stmt) => for {
+          l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+        } yield Labeled(id, EnhancedFor(modifiers, ty, id, exp, stmt))
+        case Do(stmt, exp) => for {
+          l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+        } yield Labeled(id, Do(l_stmt, exp))
+        case While(exp, stmt) => for {
+          l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+        } yield Labeled(id, While(exp, l_stmt))
+        case Switch(exp, blocks) => for {
+          l_blocks <- blocks.traverse(labelOps.label(_, Some(id), labelC))
+        } yield Labeled(id, Switch(exp, l_blocks))
+        case _ => m.raiseError("Labelling failed. A labeled statement should be a loop or a switch statement, but some other type of statement is given.")
+      }
+      case BasicFor(init, loop_cond, post_update, stmt) => for {
+        id <- newLabel
+        l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+      } yield Labeled(id, BasicFor(init, loop_cond, post_update, l_stmt))
+      case EnhancedFor(modifiers, ty, id, exp, stmt) => for {
+        id <- newLabel
+        l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+      } yield Labeled(id, EnhancedFor(modifiers, ty, id, exp, stmt))
+      case Do(stmt, exp) => for {
+        id <- newLabel
+        l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+      } yield Labeled(id, Do(l_stmt, exp))
+      case While(exp, stmt) => for {
+        id <- newLabel
+        l_stmt <- labelOps.label(stmt, Some(id), Some(id))
+      } yield Labeled(id, While(exp, l_stmt))
+      case Switch(exp, blocks) => for {
+        id <- newLabel
+        l_blocks <- blocks.traverse(labelOps.label(_, Some(id), labelC))
+      } yield Labeled(id, Switch(exp, l_blocks))
+      case Break(mid) => mid match {
+        case None => labelB match { 
+          case None => m.raiseError("Labelling failed. An orphan break statement is encountered,")
+          case Some(_) => m.pure(Break(labelB))
+        }
+        case Some(id) => m.pure(Break(mid))
+      }
+      case Continue(mid) => mid match {
+        case None => labelC match {
+          case None => m.raiseError("Labelling failed. An orphan continue statement is encountered.")
+          case Some(_) => m.pure(Continue(labelC))
+        }
+        case Some(id) => m.pure(Continue(mid))
+      }
+      case Empty => m.pure(Empty)
+      case ExpStmt(exp) => m.pure(ExpStmt(exp))
+      case IfThen(exp, stmt) => for {
+        l_stmt <- labelOps.label(stmt, labelB, labelC)
+      } yield IfThen(exp, l_stmt)
+      case IfThenElse(exp, then_stmt, else_stmt) => for {
+        l_then_stmt <- labelOps.label(then_stmt, labelB, labelC)
+        l_else_stmt <- labelOps.label(else_stmt, labelB, labelC)
+      } yield IfThenElse(exp, l_then_stmt, l_else_stmt)
+      case Return(exp) => m.pure(Return(exp))
+      case StmtBlock(blk) => for {
+        l_blk <- labelOps.label(blk, labelB, labelC)
+      } yield StmtBlock(l_blk)
+      case Synchronized(exp, blk) => for {
+        l_blk <- labelOps.label(blk, labelB, labelC)
+      } yield Synchronized(exp, l_blk)
+      case Throw(exp) => m.pure(Throw(exp))
+      case Try(try_blk, catches, finally_blk) => for {
+        l_try_blk <- labelOps.label(try_blk, labelB, labelC) 
+        l_catches <- catches.traverse(labelOps.label(_, labelB, labelC))
+        l_finally_blk <- finally_blk.traverse(labelOps.label(_, labelB, labelC))
+      } yield Try(l_try_blk, l_catches, l_finally_blk)
+    }
+ 
+    implicit def switchBlockLabelInstance:Label[SwitchBlock] = new Label[SwitchBlock] {
+      override def label(a: Syntax.SwitchBlock, labelB:Option[Ident], labelC:Option[Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.SwitchBlock] = a match {
+        case SwitchBlock(sw_label, blk_stmts) => for {
+          l_blk_stmts <- blk_stmts.traverse(labelOps.label(_, labelB, labelC))
+        } yield SwitchBlock(sw_label, l_blk_stmts)
+      }
+    }
+
+    implicit def catchLabelInstance:Label[Catch] = new Label[Catch] {
+      override def label(a: Syntax.Catch, labelB: Option[Syntax.Ident], labelC: Option[Syntax.Ident])(implicit m: MonadError[LabelState,String]): LabelState[Syntax.Catch] = a match {
+        case Catch(params, blk) => for {
+          l_blk <- labelOps.label(blk, labelB, labelC) 
+        } yield Catch(params, l_blk)
+      }
+    }
+  }
 }
---
+++ src/test/scala/com/github/luzhuomi/obsidian/TestLabel.scala
@@ -0,0 +1,44 @@
+package com.github.luzhuomi.obsidian
+
+import com.github.luzhuomi.scalangj.Lexer
+import com.github.luzhuomi.scalangj.Parser._
+import com.github.luzhuomi.scalangj.Syntax._
+import com.github.luzhuomi.scalangj.Pretty._
+import com.github.luzhuomi.obsidian._ 
+import com.github.luzhuomi.obsidian.Label._
+
+import org.scalatest.{FunSuite, Matchers}
+
+
+class TestLabel1 extends FunSuite with Matchers {
+    val METHODSTR = """
+public static void main(String [] args) {
+	int x = 0;
+	while (x > 10) {
+        System.out.println(x);
+        x++;
+    }
+}
+    """
+    val L_METHODSTR = """
+ public static void main (String[] args)
+{ int x = 0; obsLbl_0: while (x > 10) { System.out.println(x); x++; } }   
+    """
+    val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
+    val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
+    test("TestLabel1") {
+        methoddecl match {
+            case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
+              labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
+                  case LabelError(message) => fail(message)
+                  case LabelOk((st, l_methodDecl)) => {
+                      val result:Decl = MemberDecl_(l_methodDecl)
+                      // println(prettyPrint(result))
+                      assert(result == l_methoddecl)
+                  }
+              }
+            }
+            case _ => fail("It is supposed to be a MethodDecl member, but some other type is encountered.")           
+        }
+    }
+}
