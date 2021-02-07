1022ae6593ab387a3b6a42f48ad802cd2d343513
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2021-02-07 15:16:07 +0800

    added 4 more test cases in TestLabel.scala

--- src/test/scala/com/github/luzhuomi/obsidian/TestLabel.scala
+++ src/test/scala/com/github/luzhuomi/obsidian/TestLabel.scala
@@ -77,7 +77,7 @@ public static void main (String[] args)
                   case LabelError(message) => fail(message)
                   case LabelOk((st, methDecl)) => {
                       val result:Decl = MemberDecl_(methDecl)
-                      println(prettyPrint(result))
+                      // println(prettyPrint(result))
                       assert(result == l_methoddecl)
                   }
               }
@@ -177,6 +177,180 @@ public static void main (String[] args)
     val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
     val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
     test("TestLabel4") {
+        methoddecl match {
+            case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
+              labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
+                  case LabelError(message) => fail(message)
+                  case LabelOk((st, methDecl)) => {
+                      val result:Decl = MemberDecl_(methDecl)
+                      // println(prettyPrint(result))
+                      assert(result == l_methoddecl)
+                  }
+              }
+            }
+            case _ => fail("It is supposed to be a MethodDecl member, but some other type is encountered.")           
+        }
+    }
+}
+
+
+class TestLabel5 extends FunSuite with Matchers {
+    val METHODSTR = """
+ public static void main (String[] args)
+{ int x = 0; l_0: while (x > 10) { System.out.println(x); x++; } }   
+    """
+    val L_METHODSTR = """
+ public static void main (String[] args)
+{ int x = 0; l_0: while (x > 10) { System.out.println(x); x++; } }   
+    """
+    val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
+    val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
+    test("TestLabel5") {
+        methoddecl match {
+            case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
+              labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
+                  case LabelError(message) => fail(message)
+                  case LabelOk((st, methDecl)) => {
+                      val result:Decl = MemberDecl_(methDecl)
+                      // println(prettyPrint(result))
+                      assert(result == l_methoddecl)
+                  }
+              }
+            }
+            case _ => fail("It is supposed to be a MethodDecl member, but some other type is encountered.")           
+        }
+    }
+}
+
+
+class TestLabel6 extends FunSuite with Matchers {
+    val METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: while (true)
+  { System.out.println(x); if (x < 10) { x++; continue l_0; } else { break l_0; } }
+}
+
+    """
+    val L_METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: while (true)
+  { System.out.println(x); if (x < 10) { x++; continue l_0; } else { break l_0; } }
+}
+    """
+    val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
+    val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
+    test("TestLabel6") {
+        methoddecl match {
+            case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
+              labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
+                  case LabelError(message) => fail(message)
+                  case LabelOk((st, methDecl)) => {
+                      val result:Decl = MemberDecl_(methDecl)
+                      // println(prettyPrint(result))
+                      assert(result == l_methoddecl)
+                  }
+              }
+            }
+            case _ => fail("It is supposed to be a MethodDecl member, but some other type is encountered.")           
+        }
+    }
+}
+
+
+class TestLabel7 extends FunSuite with Matchers {
+    val METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: do
+  {
+    System.out.println(x);
+    if (x < 10)
+    { x++; continue l_0; }
+    else
+    { break l_0; }
+  } while (true);
+}
+    """
+    val L_METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: do
+  {
+    System.out.println(x);
+    if (x < 10)
+    { x++; continue l_0; }
+    else
+    { break l_0; }
+  } while (true);
+}
+    """
+    //println(classBodyStatement.apply(new Lexer.Scanner(METHODSTR)))
+    val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
+    val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
+    test("TestLabel7") {
+        methoddecl match {
+            case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
+              labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
+                  case LabelError(message) => fail(message)
+                  case LabelOk((st, methDecl)) => {
+                      val result:Decl = MemberDecl_(methDecl)
+                      // println(prettyPrint(result))
+                      assert(result == l_methoddecl)
+                  }
+              }
+            }
+            case _ => fail("It is supposed to be a MethodDecl member, but some other type is encountered.")           
+        }
+    }
+}
+
+class TestLabel8 extends FunSuite with Matchers {
+    val METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: switch (x)
+  {
+    case 1:
+      x++;
+      break l_0;
+    case 2:
+      x--;
+      break l_0;
+    default:
+      System.out.println(x);
+  }
+  ;
+}
+    """
+    val L_METHODSTR = """
+public static void main (String[] args)
+{
+  int x = 0;
+  l_0: switch (x)
+  {
+    case 1:
+      x++;
+      break l_0;
+    case 2:
+      x--;
+      break l_0;
+    default:
+      System.out.println(x);
+  }
+  ;
+}
+    """" 
+    //println(classBodyStatement.apply(new Lexer.Scanner(METHODSTR)))
+    val methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(METHODSTR)).get.get 
+    val l_methoddecl:Decl = classBodyStatement.apply(new Lexer.Scanner(L_METHODSTR)).get.get 
+    test("TestLabel8") {
         methoddecl match {
             case MemberDecl_(methodDecl@MethodDecl(_,_,_,_,_,_,_,_)) => {
               labelOps.label(methodDecl,None,None).run(Label.initStateInfo) match {
