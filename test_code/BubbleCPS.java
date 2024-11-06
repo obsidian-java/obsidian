c7aed0ece31d12df6f9f95db0d0f429bd2321df7
Timothy Zheng <timothy.zheng@uwaterloo.ca>
2024-11-06 15:37:34 +0800

    add back test code snippets

---
+++ test_code/BubbleCPS.java
@@ -0,0 +1,294 @@
+import java.util.function.*;
+import java.util.Map;
+import java.util.HashMap;
+
+public class BubbleCPS {
+    public static void main(String [] argv) {
+		int [] nums = {13, 3, 4, 14, 67, 45, 34};
+		bubble(nums);
+		for (int i = 0; i < nums.length; i ++) {
+			System.out.println(nums[i]);
+		}
+    }
+
+
+    public static Function<Function<Exception, Void>, Function<Function<Void, Void>, Void>> 
+    loop(Function<Void, Boolean> cond, 
+            Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>> visitor,
+            Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>> exit) { 
+        return raise -> k -> { 
+                if (cond.apply(null)) {
+                    return visitor.apply(raise).apply( isNull -> 
+                        loop(cond, visitor, exit).apply(raise).apply(k)
+                    );
+                } else {
+                    return exit.apply(raise).apply(k);
+                }
+            };
+        }
+    
+
+    
+    public static Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> 
+    trycatch (Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>> visitor,
+              Function<Exception, Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>>> handler) {
+        return raise -> k -> 
+            visitor.apply(ex -> handler.apply(ex).apply(raise).apply(k)).apply(k);
+    
+    }
+
+    public static Function<Function<Exception,Void>, Function<Function<Void,Void>, Void>> 
+    seq (Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>> first,
+        Function<Function<Exception, Void>, Function<Function<Void,Void>, Void>> second) {
+        return raise -> k -> 
+            first.apply(raise).apply(isnull -> second.apply(raise).apply(k));
+    }
+
+
+    public static Function<Function<Exception,Void>, Function<Function<Void,Void>, Void>>
+    ifelse (Function<Void, Boolean> cond, 
+    Function<Function<Exception,Void>, Function<Function<Void,Void>, Void>> th, 
+    Function<Function<Exception,Void>, Function<Function<Void,Void>, Void>> el) {
+        return raise -> k -> {
+                if (cond.apply(null)) {
+                    return th.apply(raise).apply(k);
+                } else {
+                    return el.apply(raise).apply(k);
+                }
+        };
+    }
+
+    public static Function<Void, Void> id = (isNull) -> {
+        return null;
+    };
+
+    public static Function<Exception, Void> idHandler = e -> {
+        System.out.println(e.toString());
+        return null;
+    };
+
+
+	public static class Ctxt {
+		int t_0; // 0
+		boolean changed_0 = false;
+		int i_0;
+		int j_0;
+		int i_1, i_1p, i_2 ,i_3, i_3p, i_4, i_8, i_11;
+		int j_1, j_1p, j_2, j_3, j_3p, j_4, j_7, j_8;
+		int t_1, t_1p, t_3, t_3p, t_5, t_4, t_8;
+	    boolean changed_1, changed_1p, changed_2, changed_3, changed_3p, changed_5, changed_4, changed_8;
+        HashMap<Integer, Function<Exception,Void>> raise_map;
+        HashMap<Integer, Function<Void,Void>> k_map;
+        HashMap<Integer, Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>>> func_map;
+        public Ctxt() {
+            this.raise_map = new HashMap<>();
+            this.k_map = new HashMap<>();
+            this.func_map = new HashMap<>();
+        }
+	}
+
+
+
+	
+    public static void bubble(int [] nums) {
+
+		final Ctxt ctxt = new Ctxt();
+
+		Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble0 = raise -> k -> {
+            ctxt.changed_0 = false;
+            ctxt.i_0 = nums.length - 1;
+            return k.apply(null);
+        };
+
+ 
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k01 = raise -> k -> {
+            ctxt.i_1 = ctxt.i_0;
+            ctxt.j_1 = ctxt.j_0;
+            ctxt.t_1 = ctxt.t_0;
+            ctxt.changed_1 = ctxt.changed_0;
+    
+            return k.apply(null);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k11 = raise -> k -> {
+            ctxt.i_1p = ctxt.i_1;
+            ctxt.j_1p = ctxt.j_1;
+            ctxt.changed_1p = ctxt.changed_1;
+            ctxt.t_1p = ctxt.t_1;
+            return k.apply(null);
+        };
+
+                
+        // block 2
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble2 = raise -> k -> {
+            System.out.println("bubble2");
+            ctxt.changed_2 = false;
+            ctxt.j_2 = 0;
+            return k.apply(null);
+        };
+
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k23 = raise -> k -> {
+            ctxt.i_3 = ctxt.i_1;
+            ctxt.j_3 = ctxt.j_2;
+            ctxt.t_3 = ctxt.t_1;
+            ctxt.changed_3 = ctxt.changed_2;
+            return k.apply(null);
+        };
+
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble5 = raise -> k -> {
+            ctxt.t_5 = nums[ctxt.j_3+1];
+            nums[ctxt.j_3+1] = nums[ctxt.j_3];
+            nums[ctxt.j_3] = ctxt.t_5;
+            ctxt.changed_5 = true;
+            return k.apply(null); 
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k54 = raise -> k -> {
+            ctxt.j_4 = ctxt.j_3;
+            ctxt.changed_4 = ctxt.changed_5;
+            ctxt.i_4 = ctxt.i_3;
+            ctxt.t_4 = ctxt.t_5;
+            return k.apply(null);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble6 = raise -> k -> {
+            return k.apply(null); // supposed to be empty
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k64 = raise -> k -> {
+            ctxt.j_4 = ctxt.j_3;
+            ctxt.changed_4 = ctxt.changed_3;
+            ctxt.i_4 = ctxt.i_3;
+            ctxt.t_4 = ctxt.t_3;
+            return k.apply(null);
+        };
+        
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble4 = raise -> k -> {
+            Function<Void, Boolean> cond = isnull -> new Boolean(nums[ctxt.j_3] > nums[ctxt.j_3+1]);
+            return ifelse(cond, seq(bubble5,k54), seq(bubble6, k64)).apply(raise).apply(k);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble7 = raise -> k -> {
+            ctxt.j_7 = ctxt.j_4 + 1;
+            return k.apply(null); 
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k73 = raise -> k -> {
+            ctxt.i_3 = ctxt.i_4;
+            ctxt.j_3 = ctxt.j_7;
+            ctxt.t_3 = ctxt.t_4;
+	    ctxt.changed_3 = ctxt.changed_4;
+            return k.apply(null);
+        };
+
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k33 = raise -> k -> {
+            ctxt.i_3p = ctxt.i_3;
+            ctxt.j_3p = ctxt.j_3;
+            ctxt.t_3p = ctxt.t_3;
+            ctxt.changed_3p = ctxt.changed_3;
+            return k.apply(null);
+        };
+
+        // k91 is the merge at the end of while loop (block 1)
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k91 = raise -> k -> {
+            ctxt.i_1p = ctxt.i_3p;
+            ctxt.j_1p = ctxt.j_3p;
+            ctxt.changed_1p = ctxt.changed_3p;
+            ctxt.t_1p = ctxt.t_3p;
+            return k.apply(null);
+        };
+        // block 9 is a break statement
+        // PROBLEM, bubble12 will continue with the current continuation
+        // we need to save the continuations at the point of the loop is first entered.
+        /*
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble9 = raise -> k -> {
+            return bubble12.apply(raise).apply(k); 
+        }
+        */
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble9 = raise -> k -> {
+            // we look up the continuation context;
+            Function<Exception,Void> raise1 = ctxt.raise_map.get(1);
+            Function<Void,Void> k1 = ctxt.k_map.get(1); 
+            Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> exp = ctxt.func_map.get(1);
+            System.out.println("bubble9");
+            return exp.apply(raise1).apply(k1); 
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble10 = raise -> k -> {
+            return k.apply(null);
+        };
+
+        // phi resolution from 10 to 8
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k108 = raise -> k -> {
+            ctxt.changed_8 = ctxt.changed_3p;
+            ctxt.i_8 = ctxt.i_3p;
+            ctxt.j_8 = ctxt.j_3p;
+            ctxt.t_8 = ctxt.t_3p;
+            return k.apply(null);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble8 = raise -> k -> {
+            Function<Void, Boolean> cond = isnull -> new Boolean(!(ctxt.changed_3p));
+            // Function<Void, Boolean> cond = isnull -> new Boolean(false); 
+
+            return ifelse(cond, seq(k91, bubble9), seq(bubble10, k108)).apply(raise).apply(k);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble11 = raise -> k -> {
+            ctxt.i_11 = ctxt.i_8 - 1;
+            return k.apply(null);
+        };
+
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> k111 = raise -> k -> {
+            ctxt.i_1 = ctxt.i_11;
+            ctxt.j_1 = ctxt.j_8;
+            ctxt.changed_1 = ctxt.changed_8;
+            ctxt.t_1 = ctxt.t_8;
+            return k.apply(null);
+        };
+
+        // named loop in block 3
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> loop3 = raise -> k -> {
+            Function<Void, Boolean> cond = isnull -> new Boolean(ctxt.j_3 < ctxt.i_3);
+            return loop(cond, seq(bubble4,seq(bubble7,k73)), seq(k33,seq(bubble8, seq(bubble11, k111)))).apply(raise).apply(k);
+        };
+
+        // block 3
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble3 = raise -> k -> {
+            return seq(k23,loop3).apply(raise).apply(k);
+        };
+        
+
+                
+        // block 12
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble12 = raise -> k -> {
+            return k.apply(null);
+        };
+        
+        // named loop in block 1
+        Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> loop1 = raise -> k -> {
+            
+            ctxt.raise_map.put(1, raise);
+            ctxt.k_map.put(1,k);
+            ctxt.func_map.put(1,bubble12);
+            Function<Void, Boolean> cond = isnull -> new Boolean(ctxt.i_1 > 0);
+            return loop(cond, seq(bubble2, bubble3), seq(k11, bubble12)).apply(raise).apply(k);
+        };
+
+        // block 1
+		Function<Function<Exception,Void>, Function<Function<Void, Void>, Void>> bubble1 = raise -> k -> {
+            System.out.println("bubble1");
+            return seq(k01, loop1).apply(raise).apply(k);
+        };
+
+
+        
+        seq(bubble0,bubble1).apply(idHandler).apply(id);
+
+    }
+
+}
---
+++ test_code/BubbleSSA.java
@@ -0,0 +1,164 @@
+
+public class BubbleSSA {
+    public static void main(String [] argv) {
+		int [] nums = {13, 3, 4, 14, 67, 45, 34};
+		bubble(nums);
+		for (int i = 0; i < nums.length; i ++) {
+			System.out.println(nums[i]);
+		}
+    }
+
+	public static void bubble(int [] nums) {
+
+		int t_0; // 0
+		boolean changed_0 = false;
+		int i_0 = nums.length - 1;
+		int j_0;
+		int i_1, i_1p, i_2 ,i_3, i_3p, i_4, i_8, i_11;
+		int j_1, j_1p, j_2, j_3, j_3p, j_4, j_7, j_8;
+		int t_1, t_1p, t_3, t_3p, t_5, t_4, t_8;
+		boolean changed_1, changed_1p, changed_2, changed_3, changed_3p, changed_5, changed_4, changed_8;
+		// need to initialized, otherwise java compiler complains that these variables 
+		// might not be initialized
+		j_0 = 0;
+		t_0 = 0;
+		changed_3p = false;
+		i_3p = 0;
+		j_3p = 0;
+		t_3p = 0;
+
+		/* join { 
+			i_1 = 0:i_0, 11: i_11
+			j_1 = 0:j_0, 11: j_8
+			t_1 = 0:t_0; 11: t_8
+			changed_1 = 0:changed_0, 11: changed_8
+		   }
+		*/
+		// phi resolution
+		i_1 = i_0;
+		j_1 = j_0;
+		t_1 = t_0;
+		changed_1 = changed_0;
+		while(i_1 > 0) { // 1 
+			changed_2 = false; // 2
+			j_2 = 0; 
+			/*
+			join {
+				i_3 = 2: i_1, 7: i_4
+				j_3 = 2: j_2, 7: j_7
+				t_3 = 2: t_1, 7: t_4
+				changed_3 = 2: changed_2, 7: changed_4 
+			}
+			*/
+			// phi resolution
+			i_3 = i_1;
+			j_3 = j_2;
+			t_3 = t_1;
+			changed_3 = changed_2;
+			while(j_3 < i_3) { // 3
+				if (nums[j_3] > nums[j_3+1]) { // 4
+					t_5 = nums[j_3+1];         // 5
+					nums[j_3+1] = nums[j_3];
+					nums[j_3] = t_5;
+					changed_5 = true;
+					// phi resolution
+					j_4 = j_3;
+					changed_4 = changed_5;
+					i_4 = i_3;
+					t_4 = t_5;
+				} else {
+					// 6
+					// phi resolution
+					j_4 = j_3;
+					changed_4 = changed_3;
+					i_4 = i_3;
+					t_4 = t_3;
+				}
+				/*
+				join {
+					j_4 = 5:j_3, 6:j_3
+					changed_4 = 5:changed_5, 6:changed_3
+					i_4 = 5:i_3, 6:i_3
+					t_4 = 5:t_5, 6:t_3
+				}
+				*/
+				j_7 = j_4 + 1; // 7 // stopped here
+				// phi resolution for start of while
+				i_3 = i_4;
+				j_3 = j_7;
+				t_3 = t_4;
+				changed_3 = changed_4;				
+				// phi resolution for end of while 
+				// only applicable when the loop condition is not satisfied
+				if (!(j_3 < i_3)) {
+					i_3p = i_3;
+					j_3p = j_3;
+					t_3p = t_3;
+					changed_3p = changed_3;
+				}
+			}
+			/*
+			join {
+				i_3p = 3:i_3,
+				j_3p = 3:j_3
+				t_3p = 3:t_3
+				changed_3p = 3:changed_3
+			}
+			*/
+			// System.out.println(i_3p);
+			// optimization
+			if (!changed_3p) { // 8
+				i_1p = i_3p;
+				j_1p = j_3p;
+				changed_1p = changed_3p;
+				t_1p = t_3p;
+				break; // 9
+			} else {
+				// 10
+				// phi resolution
+				changed_8 = changed_3p;
+				i_8 = i_3p;
+				j_8 = j_3p;
+				t_8 = t_3p;
+			} 
+			/*
+			join {
+				changed_8 = 10: changed_3p
+				i_8 = 10: i_3p
+				j_8 = 10: j_3p
+				t_8 = 10: t_3p
+			} 
+			*/
+			i_11 = i_8 - 1; // 11
+
+
+			// phi resolution for start of while join
+			i_1 = i_11;
+			j_1 = j_8;
+			changed_1 = changed_8;
+			t_1 = t_8;
+			// phi resolution for end of while join
+			// this should only be fired when the loop condition is false
+			if (!(i_1 > 0)) {
+				i_1p = i_1;
+				j_1p = j_1;
+				changed_1p = changed_1;
+				t_1p = t_1;
+			}
+
+		} // end of while
+		/*
+		join {
+			changed_1p = 9: changed_3p, 1: changed_1,
+			i_1p = 9: i_3p, 1: i_1,
+			j_1p = 9: j_3p, 1: j_1,
+			t_1p = 9: t_3p, 1: t_1
+		}
+		*/
+		// phi resolution
+
+		// 12
+		return;
+    }
+
+}
